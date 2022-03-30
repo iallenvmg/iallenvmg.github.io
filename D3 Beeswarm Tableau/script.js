461614a9506d1b860c51e3cb0f40237c8ce7eef9
iallenvmg <99823319+iallenvmg@users.noreply.github.com>
2022-03-30 16:19:06 -0400

    Update script.js

--- D3 Beeswarm Tableau/script.js
+++ D3 Beeswarm Tableau/script.js
@@ -32,7 +32,7 @@
 	  var indexColumns = settings.selectedColumns[1];
 	  var indexRadius = settings.selectedRadius[1];
 	  var indexValues = settings.selectedValues[1];
-	  var colorDict = settings.selectedColorDict.split(',');
+	  var colorDict = settings.selectedColorDict.replace('[','').replace(']','').split(',');
 	  console.log(colorDict)
 	  let dataArr = [];
 	  console.log(indexColumns);
